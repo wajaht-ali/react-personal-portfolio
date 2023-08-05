903ded4ef282d68591cd93b851d18911f7841e05
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-08-05 16:15:11 +0500

    Timeline-component updated

---
+++ src/components/Services.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div id='services'>
+        <h2>Services</h2>
+        <section>
+            <Motion.dev className="serviceBox1">
+                <h3>2+</h3>
+                <p>Years of experience</p>
+            </Motion.dev>
+
+            <Motion.dev className="serviceBox1">
+                <h3>2+</h3>
+                <p>Years of experience</p>
+            </Motion.dev>
+
+            <Motion.dev className="serviceBox1">
+                <h3>2+</h3>
+                <p>Years of experience</p>
+            </Motion.dev>
+            
+            <Motion.dev className="serviceBox1">
+                <h3>2+</h3>
+                <p>Years of experience</p>
+            </Motion.dev>
+        </section>
+    </div>
+  )
+}
+
+export default Services
