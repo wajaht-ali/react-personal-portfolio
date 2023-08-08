2e1347ab4f483a5af212347219b4d5a168dc53d6
Wajahat-404 <109886908+Wajahat-404@users.noreply.github.com>
2023-08-08 17:11:03 +0500

    Timeline bugs fixed

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,29 +4,54 @@ const Timeline = () => {
   return (
     <div id='timeline'>
         <div id="timelinebox">
-            {
+
+            <div className="timeLineItem leftTimeline">
+            <h2>TECHY STAR</h2>
+            <p>07/10/2022</p>
+            </div>
+
+            <div className="timeLineItem  rightTimeline">
+            <h2>XCRYPTP</h2>
+            <p>07/10/2022</p>
+            </div>
+
+            <div className="timeLineItem leftTimeline">
+            <h2>PORTFOLIO</h2>
+            <p>07/10/2022</p>
+            </div>
+
+            <div className="timeLineItem rightTimeline">
+            <h2>VIDEO HUB</h2>
+            <p>07/10/2022</p>
+            </div>
+
+            <div className="timeLineItem leftTimeline">
+            <h2>REACT CART</h2>
+            <p>07/10/2022</p>
+            </div>
+            {/* {
                 data.projects.map((item, index) => (
                     <TimelineItem 
                     heading={item.title}
                     text = {item.date}
-                    key = {index}
+                    index = {index}
                     />
                 ))
-            };
+            }; */}
         </div>
     </div>
   )
 }
 
-const TimelineItem = ({heading, text, index}) => (
-    <div 
-    //" '${ '}' "
-    className={'timelineItem ${ index % 2 === 0 ? "leftTimeline" : "rightTimeline"} '}>
+// const TimelineItem = ({heading, text, index}) => (
+//     <div 
+//     //" '${ '}' "
+//     className={'timelineItem  ${ index % 2 === 0 ? "leftTimeline" : "rightTimeline"}'}>
         
-        <div>
-        <h2>{heading}</h2>
-        <p>{text}</p>
-        </div>
-    </div>
-)
+//         <div>
+//         <h2>{heading}</h2>
+//         <p>{text}</p>
+//         </div>
+//     </div>
+// )
 export default Timeline;
